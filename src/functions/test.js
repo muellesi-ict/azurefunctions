e7dd66a9e8875de969b801327095368683b9bce0
muellesi-ict <simon.mueller@ict-campus.net>
2025-06-05 13:44:51 +0200

    db query function

--- src/functions/test.js
+++ src/functions/test.js
@@ -1,13 +1,24 @@
 const { app } = require('@azure/functions');
 
-app.http('test', {
-    methods: ['GET', 'POST'],
+const { app, input } = require('@azure/functions');
+
+const cosmosInput = input.cosmosDB({
+    databaseName: 'DemoDatabase',
+    containerName: 'Items',
+    connection: 'CosmosDB',
+    sqlQuery: "select * from c"
+});
+
+app.http('getItems', {
+    methods: ['GET'],
     authLevel: 'anonymous',
+    extraInputs: [cosmosInput],
+    route: 'items',
     handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
-
-        const name = request.query.get('name') || await request.text() || 'world';
-
-        return { body: `Hello, ${name}!` };
+        const items = context.extraInputs.get(cosmosInput);
+        return {
+            body: JSON.stringify(items),
+            status: 200
+        };
     }
 });
